1162dcb9f6670d3c29c221a037cc895970b5be27
马营屹 <mayingyi@mayingyideMacBook-Pro.local>
2018-03-27 21:59:33 +0800

    md5

--- src/main/java/com/mmall/common/Const.java
+++ src/main/java/com/mmall/common/Const.java
@@ -3,4 +3,18 @@ package com.mmall.common;
 public class Const {
 
     public static final String CURRENT_USER = "currentUser";
+
+    public static final String EMAIL = "email";
+
+    public static final String USERNAME = "username";
+
+    /**
+     * 接口类对常量进行分组
+     * 不枚举类体积小起到分组的作用
+     */
+    public interface Role{
+        int ROLE_CUSTOMER = 0; //普通用户
+        int ROLE_ADMIN = 1; //管理员
+    }
+
 }
